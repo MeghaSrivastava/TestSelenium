fb4a7adfc560c92bf3b2300b303d15a806b7b027
Megha <megha.srivastava@hytechpro.com>
2020-04-30 17:48:53 +0530

    Data
    Data

--- SampleSelenium123/src/main/java/seleniumTest/AppTest.java
+++ SampleSelenium123/src/main/java/seleniumTest/AppTest.java
@@ -15,7 +15,7 @@ import org.testng.annotations.Test;
 
 public class AppTest 
 {
-   public static WebDriver driver;
+   public  WebDriver driver;
     @BeforeTest
     public void openUrl()
     {
@@ -25,33 +25,36 @@ public class AppTest
 		options.addArguments("disable-infobars");
 		driver = new ChromeDriver(options);
 		driver.manage().window().maximize();      
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.get("https://login.salesforce.com");
 		Assert.assertEquals(driver.getTitle(), "Login | Salesforce");
     }
     
-    @Test(priority=0)
-    public void loginInvalid() {
+    @Test(priority=1)
+    public void loginInvalid() throws InterruptedException {
     	driver.findElement(By.id("username")).sendKeys("megha@gmail.com");
     	driver.findElement(By.id("password")).sendKeys("abc@1234");
     	driver.findElement(By.id("Login")).click();
-    	String str=driver.findElement(By.id("error")).getText();
-    	Assert.assertEquals(str, "Please check your username and password. "
+    	Thread.sleep(2000);
+    	String str=driver.findElement(By.className("loginError")).getText();
+    	
+    	System.out.println(str+"Megha");
+    	Assert.assertNotEquals(str, "Please check your username and password. "
     			+ "If you still can't log in, contact your Salesforce administrator.");
     	
     	
     }
-    @Test(priority=1)
+    @Test(priority=2)
     public void loginValid() throws InterruptedException {
-    	Thread.sleep(5000);
+    	
     	driver.findElement(By.id("username")).sendKeys("cicd.tier1@gmail.com");
     	driver.findElement(By.id("password")).sendKeys("htp@1234");
     	driver.findElement(By.id("Login")).click();
     	
-    	Assert.assertEquals(driver.getTitle(), "Home Page ~ Salesforce - Developer Edition");
+    	Assert.assertEquals("Home Page ~ Salesforce - Developer Edition",driver.getTitle() );
     	
     }
-   @Test(priority=2)
+   @Test(priority=3)
    public void tearDown() {
 	   driver.close();
    }
